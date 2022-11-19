3d9e61e550d80b2222e24c09609ab40b7d086738
Orkhan Shahverdiyev <olik@unec.edu.az>
2022-11-19 16:09:36 +0400

    Last version

--- todo.controller.js
+++ todo.controller.js
@@ -6,69 +6,66 @@ class todoController {
     constructor() {
         this.model = new todoModel()
         this.view = new todoView()
-        this.view.showTodos(this.model.todos)
         this.eventListener()
+    }
+
+
+    eventListener(){
+        this.view.rightCurrency(document.querySelector('.convert__to_list .convert__list_selected'))
+        this.view.leftCurrency(document.querySelector('.convert__from_list .convert__list_selected'))
         this.navbarListener()
+        this.currencyListener()
+        this.inputListener()
     }
 
     navbarListener(){
         document.querySelectorAll('.navbar__list_item').forEach((element)=>{
             element.addEventListener('click', ()=>{
-                document.querySelector('.navbar__list_item-selected').className = "navbar__list_item"
-                element.className = "navbar__list_item navbar__list_item-selected" 
+                this.view.navbarItemColor(element)
+            })
+        })
+    }
+
+
+    currencyListener(){
+        document.querySelectorAll('.convert__list_item').forEach(element =>{
+            element.addEventListener('click', ()=>{
+                if(element.parentElement.className.includes('from'))
+                {
+                    this.view.leftCurrency(element)
+                }
+                else if(element.parentElement.className.includes('to'))
+                {
+                    this.view.rightCurrency(element)
+                }  
+            })
+        })
+    }
+                
+    inputListener(){
+        document.querySelectorAll('input').forEach((element)=>{
+            element.addEventListener('input', (event)=>{
+                let key = event.data
+                if(key==","){
+                    key = '.'
+                }
+                if(('0123456789'.includes(key))||key==null){
+                    this.view.changeInput(element)
+                }
+                if(!('0123456789'.includes(key))&&key!=null){
+                    element.value = element.value.substring(0, element.value.length - 1)
+                }
+                if(element.value=='00'||(element.value=='0'+key&&key!='0')){
+                    element.value = key
+                }
+                if(key=="."){
+                    if(!element.value.includes('.')){
+                        element.value = element.value + '.'
+                    }
+                }
                 
             })
         })
-    }
-
-    eventListener(){
-        document.querySelector('main').addEventListener('click', (event)=>{
-            if(event.target.className=="container__button_text" || event.target.className=="container__button_first" 
-            || event.target.className=="container__button_second"){
-                this.addButton()
-            }
-            else if(event.target.className=="container__todos_delete"){
-                this.deleteButton(event.target)
-            }
-            else if(event.target.className=="container__todos_input"){
-                this.inputListener()
-            }
-            else if(event.target.className.includes("container__sort")){
-                this.sortButton()
-            }
-        })
-    }
-
-    inputListener(){
-        document.querySelectorAll('.container__todos_input').forEach((element)=>{
-            element.addEventListener('input', (event)=>{
-                this.editButton(element, event.target.value)
-            })
-        })
-    }
-    addButton(){
-        this.model.addTodo()
-    }
-
-    sortButton(){
-        if(document.querySelector('.container__sort').className =="container__sort container__sort_asc")
-        {
-            document.querySelector('.container__sort').classList = 'container__sort container__sort_desc'
-            this.model.sortTodosAsc()
-        } 
-        else
-        {
-            document.querySelector('.container__sort').classList = "container__sort container__sort_asc"
-            this.model.sortTodosDesc()
-        }
-    }
-
-    deleteButton(event){
-        this.model.deleteTodo(Array.from(event.parentNode.parentNode.children).indexOf(event.parentNode)+1)
-    }
-
-    editButton(event, eventl){
-        this.model.editTodo(Array.from(event.parentNode.parentNode.children).indexOf(event.parentNode)+1, eventl)        
     }
   }
 
